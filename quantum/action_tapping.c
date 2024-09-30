7e53e85f3171720470b5a11ee590534c12abf04e
sekigon-gonnoc <43873124+sekigon-gonnoc@users.noreply.github.com>
2024-09-30 20:23:02 +0900

    Fix HOLD behavior of slave side

--- quantum/action_tapping.c
+++ quantum/action_tapping.c
@@ -9,6 +9,10 @@
 #include "quantum_keycodes.h"
 #include "timer.h"
 
+#ifdef PROTOCOL_BMP
+#    include "bmp/state_controller.h"
+#endif
+
 #ifndef NO_ACTION_TAPPING
 
 #    if defined(IGNORE_MOD_TAP_INTERRUPT_PER_KEY)
@@ -257,6 +261,9 @@ bool process_tapping(keyrecord_t *keyp) {
             process_record_tap_hint(&tapping_key);
             waiting_buffer_scan_tap();
             debug_tapping_key();
+#    ifdef PROTOCOL_BMP
+            BMPAPI->app.schedule_next_task(GET_TAPPING_TERM(keyp->keycode, keyp) + 1);
+#    endif
         } else {
             // the current key is just a regular key, pass it on for regular
             // processing
